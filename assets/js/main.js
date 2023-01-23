406aa759ad4cca234618dcfe56517ef7d22d660b
karinlee <karinlee92@gmail.com>
2023-01-23 13:41:30 +0100

    initial

---
+++ assets/js/main.js
@@ -0,0 +1,21 @@
+const x = 27;
+
+function difference(num){
+    let diff = num - x;
+    let result = diff * 2;
+
+    if (num > 27){
+    }
+
+    else{
+    result = "too low";
+    }
+
+    console.log(result);
+}
+
+difference(35);
+difference(74);
+difference(123);
+
+difference(25);
